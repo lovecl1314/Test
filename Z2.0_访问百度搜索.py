54f316a066e377f7ddc398e8568c7484c42f05f3
Ziloo <126108160@qq.com>
2018-11-30 23:30:23 +0800

    Update Z2.0_访问百度搜索.py

--- Z2.0_访问百度搜索.py
+++ Z2.0_访问百度搜索.py
@@ -11,7 +11,7 @@ url = 'http://www.baidu.com'
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get(url)
-# driver.find_element_by_name('wd').send_keys(u'厦门国贸集团股份有限公司') #u'厦门国贸集团股份有限公司'
+# driver.find_element_by_name('wd').send_keys(u'厦门建发股份有限公司') #u'厦门建发股份有限公司'
 
 # 写法1 根据表单元素提交
 driver.find_element_by_name('wd').submit()
@@ -20,7 +20,7 @@ driver.find_element_by_name('wd').submit()
 # elem = driver.find_element_by_name('wd')
 # elem.send_keys(Keys.RETURN)
 # elem = driver.find_element_by_name('wd')
-# elem.send_keys(u'厦门国贸集团股份有限公司',Keys.RETURN)  #Keys.ARROW_DOWN
+# elem.send_keys(u'厦门建发股份有限公司',Keys.RETURN)  #Keys.ARROW_DOWN
 # # copy content of A
 # dr.find_element_by_id('A').send_keys((Keys.CONTROL, 'a'))
 # dr.find_element_by_id('A').send_keys((Keys.CONTROL, 'x'))
@@ -39,7 +39,7 @@ time.sleep(3)
 
 # time.sleep(3)
 # print(u"刚才访问的网站是 {0},网站标题是： {1}".format(url.decode('utf-8'),driver.title) )
-# search = '厦门国贸集团股份有限公司'
+# search = '厦门建发股份有限公司'
 # search2 = search.decode('utf-8')
 # time.sleep(3)
 # driver.quit()
